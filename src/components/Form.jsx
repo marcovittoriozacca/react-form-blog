bdd14686f760530c09bca25aa59836122a4a4a21
marcovittoriozacca <zaccamarco01@gmail.com>
2024-06-18 13:34:58 +0200

    fix: handleSubmit changed position

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,14 +6,14 @@ const Form = () => {
     }
 
     return(
-        <form>
+        <form onClick={(e) => handleSubmit(e)}>
             {/* title input field */}
             <div className="flex items-center gap-x-5 p-5 bg-neutral-400">
                 <label htmlFor="title">Title</label>
                 <input type="text" id="title"/>
             </div>
 
-            <button type="submit" onClick={(e) => handleSubmit(e)}>Create</button>
+            <button type="submit">Create</button>
         </form>
     )
 }
