3c857be54908eaf1593c9343f7058c9d45de1426
marcovittoriozacca <zaccamarco01@gmail.com>
2024-06-18 13:56:07 +0200

    feat: title state with onChange input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,27 @@
+import { useState } from "react";
 
 const Form = () => {
 
+    //prevent the form from reloading the page on submit
     const handleSubmit = (e) => {
         e.preventDefault();
     }
+
+    // title state with setTitle
+    const [title, setTitle] = useState('');
+
 
     return(
         <form onClick={(e) => handleSubmit(e)}>
             {/* title input field */}
             <div className="flex items-center gap-x-5 p-5 bg-neutral-400">
                 <label htmlFor="title">Title</label>
-                <input type="text" id="title"/>
+                <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
             </div>
 
             <button type="submit">Create</button>
         </form>
+
     )
 }
 
