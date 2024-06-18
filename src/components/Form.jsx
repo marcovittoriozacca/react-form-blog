d4f67f2f67c15fe58a81e87bf432159219f362e3
marcovittoriozacca <zaccamarco01@gmail.com>
2024-06-18 13:34:23 +0200

    feat: Form starting point with a label and an input field

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,21 @@
+
 const Form = () => {
-    return(<>
-    
-    </>)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    return(
+        <form>
+            {/* title input field */}
+            <div className="flex items-center gap-x-5 p-5 bg-neutral-400">
+                <label htmlFor="title">Title</label>
+                <input type="text" id="title"/>
+            </div>
+
+            <button type="submit" onClick={(e) => handleSubmit(e)}>Create</button>
+        </form>
+    )
 }
 
 export default Form;
